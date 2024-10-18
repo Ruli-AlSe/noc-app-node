8e58438cc338a398d82e8b27b14d9647cc75e687
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-18 03:18:51 -0600

    Use log repository in check service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,9 @@
 import { CheckService } from '../domain/use-cases/checks/check.service';
+import { FileSystemDataSource } from '../infrastructure/datasources/file-system.datasource';
+import { LogRepositoryImplementation } from '../infrastructure/repositories/log.respository';
 import { CronService } from './cron/cron.service';
+
+const fileSystemLogRepository = new LogRepositoryImplementation(new FileSystemDataSource());
 
 export class Server {
   public static start() {
@@ -8,6 +12,7 @@ export class Server {
 
     CronService.createJob('*/5 * * * * *', () => {
       new CheckService(
+        fileSystemLogRepository,
         () => console.log(`${url} is ok!`),
         (error) => console.log(error)
       ).execute(url);
