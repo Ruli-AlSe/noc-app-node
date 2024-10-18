9c41667e061b8c8015039bceea57587f4bd6db47
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-18 02:18:07 -0600

    Implement check service in server class to check every 5 sec

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,12 +1,16 @@
+import { CheckService } from '../domain/use-cases/checks/check.service';
 import { CronService } from './cron/cron.service';
 
 export class Server {
   public static start() {
     console.log('Server started...');
+    const url = 'http://localhost:3000/posts';
 
     CronService.createJob('*/5 * * * * *', () => {
-      const date = new Date();
-      console.log('5 seconds', date);
+      new CheckService(
+        () => console.log(`${url} is ok!`),
+        (error) => console.log(error)
+      ).execute(url);
     });
   }
 }
