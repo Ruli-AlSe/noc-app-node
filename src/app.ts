8685f7c95e228e37d525edb629719b5ddc7affac
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-18 00:53:43 -0600

    Create IIFE function in my app file to run my server

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
-export const name: string = 'Raul Almanza!!';
+import { Server } from './presentation/server';
 
-console.log(name);
+(() => {
+  main();
+})();
+
+function main() {
+  Server.start();
+}
