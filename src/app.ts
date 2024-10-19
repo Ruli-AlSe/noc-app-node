0e2b42f769a15fa2d54ad85e3c7382b4e67bbc97
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-19 03:08:50 -0600

    Save log in mongo

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,31 @@
 import 'dotenv/config';
 import { Server } from './presentation/server';
+import { MongoDatabase } from './data/mongo/init';
+import { envs } from './config/plugins/envs.plugin';
+import { LogModel } from './data/mongo/models/log.model';
 
 (() => {
   main();
 })();
 
-function main() {
-  Server.start();
+async function main() {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  });
+
+  // create a record in mongodb
+  // const newLog = await LogModel.create({
+  //   message: 'This is a test log from mongo',
+  //   origin: 'app.ts',
+  //   level: 'low',
+  // });
+
+  // await newLog.save();
+
+  // get all records from mongodb
+  const logs = await LogModel.find();
+  console.log({ logs });
+
+  // Server.start();
 }
