1e5b7828266ce014b37f3f43067c6d71c97a54ea
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-18 21:18:18 -0600

    Create a service to send emails using nodemailer

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,6 +2,7 @@ import { CheckService } from '../domain/use-cases/checks/check.service';
 import { FileSystemDataSource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImplementation } from '../infrastructure/repositories/log.respository';
 import { CronService } from './cron/cron.service';
+import { EmailService } from './email/email.service';
 
 const fileSystemLogRepository = new LogRepositoryImplementation(new FileSystemDataSource());
 
@@ -10,6 +11,16 @@ export class Server {
     console.log('Server started...');
 
     // const url = 'http://localhost:3000/posts';
+    const emailService = new EmailService();
+    emailService.sendEmail({
+      to: 'ibyzarecidence@gmail.com',
+      subject: 'System logs',
+      htmlBody: `
+          <h3>System logs - NOC app</h3>
+          <p>This is a test email</p>
+          <p>See attached logs</p>
+          `,
+    });
     const url = 'http://google.com';
 
     CronService.createJob('*/5 * * * * *', () => {
